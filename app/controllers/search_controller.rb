41d248d3a90c2120771a37316972cf1beb0b3632
Adam Hundley <adamhundley@gmail.com>
2016-04-18 10:10:47 -0600

    getting correct information from nrel

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,7 @@
 class SearchController < ApplicationController
   def index
-    params[:q]
-    require "pry"; binding.pry
-    Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?"
+    location = params[:q]
+    response = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=KM6LFqtdyAOhSx1tOcfFRXbndHnwyAO0jCUgmFA0&radius=6&limit=10&fuel_type=LPG,ELEC&location=#{location}"
+    @stations = JSON.parse(response.body)["fuel_stations"]
   end
-
-
 end
--- spec/user_searches_by_zipcode_spec.rb
+++ spec/user_searches_by_zipcode_spec.rb
@@ -10,7 +10,7 @@ describe "User searches by zip code and sees 10 closest staions", :type => :feat
     fill_in "q", with: 80203
     click_on "Locate"
     expect(current_path).to eq "/search?zip=80203"
-    # // expect(page).to have_content ""
+    expect(page).to have_content ""
     # // Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     # // And the stations should be limited to Electric and Propane
     # // And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
