051cee37094cd5a6ffcb1381333a069a73f61f87
Adam Hundley <adamhundley@gmail.com>
2016-04-18 10:27:16 -0600

    first test passing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     location = params[:q]
-    response = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=KM6LFqtdyAOhSx1tOcfFRXbndHnwyAO0jCUgmFA0&radius=6&limit=10&fuel_type=LPG,ELEC&location=#{location}"
+    response = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=KM6LFqtdyAOhSx1tOcfFRXbndHnwyAO0jCUgmFA0&radius=6&limit=10&fuel_type=LPG,ELEC&location=#{location}"
     @stations = JSON.parse(response.body)["fuel_stations"]
   end
 end
--- spec/user_searches_by_zipcode_spec.rb
+++ spec/user_searches_by_zipcode_spec.rb
@@ -9,10 +9,18 @@ describe "User searches by zip code and sees 10 closest staions", :type => :feat
     visit "/"
     fill_in "q", with: 80203
     click_on "Locate"
-    expect(current_path).to eq "/search?zip=80203"
-    expect(page).to have_content ""
-    # // Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-    # // And the stations should be limited to Electric and Propane
-    # // And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+
+    within "div#station-66897" do
+      expect(page).to have_content "UDR"
+      expect(page).to have_content "800 Acoma St, Denver CO 80204"
+      expect(page).to have_content "ELEC"
+      expect(page).to have_content "Distance: 0.3117 miles"
+    end
+
+    expect(page).to_not have_content "BD"
+    expect(page).to_not have_content "CNG"
+    expect(page).to_not have_content "E85"
+    expect(page).to_not have_content "HY"
+    expect(page).to_not have_content "LNG"
   end
 end
